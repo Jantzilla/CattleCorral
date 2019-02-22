46b1b617757358ca564be95409fccc4bf54428b5
Jantz <jantzillac.93@gmail.com>
2019-02-22 09:36:50 -0600

    Initial commit

---
+++ android/src/com/creativesource/cattlecorral/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.creativesource.cattlecorral;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.creativesource.cattlecorral.CattleCorral;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CattleCorral(), config);
+	}
+}
