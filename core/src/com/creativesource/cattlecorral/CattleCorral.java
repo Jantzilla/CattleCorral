8d9b8481c9619dd83f1151e01e59305e81a1a3b3
Jantz <jantzillac.93@gmail.com>
2019-02-22 12:14:57 -0600

    Add 15 px black circle using the "shapeRenderer"

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -2,10 +2,12 @@ package com.creativesource.cattlecorral;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class CattleCorral extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -26,6 +28,10 @@ public class CattleCorral extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(Color.BLACK);
+		shapeRenderer.circle(300,300,15);
+		shapeRenderer.end();
 	}
 	
 	@Override
