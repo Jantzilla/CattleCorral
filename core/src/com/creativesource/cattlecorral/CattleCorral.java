ccca9a8186c09df751fe2c69b72ce7f41dc929b8
Jantz <jantzillac.93@gmail.com>
2019-02-22 12:17:13 -0600

    Comment out all default "SpriteBatch" code

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -5,29 +5,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class CattleCorral extends ApplicationAdapter {
-	SpriteBatch batch;
+//	SpriteBatch batch;
 	ShapeRenderer shapeRenderer;
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+//		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
-		img = new Texture("badlogic.jpg");
+//		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
 		shapeRenderer.begin(ShapeType.Filled);
 		shapeRenderer.setColor(Color.BLACK);
 		shapeRenderer.circle(300,300,15);
@@ -36,7 +36,7 @@ public class CattleCorral extends ApplicationAdapter {
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+//		batch.dispose();
+//		img.dispose();
 	}
 }
