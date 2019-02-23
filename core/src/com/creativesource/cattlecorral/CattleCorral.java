2ad6c3a6e2c45e22488635c3ba55f1348e5a909a
Jantz <jantzillac.93@gmail.com>
2019-02-22 22:05:19 -0600

    Add new "polyline" ShapeRenderer batch to create vertex "maze"

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -13,11 +13,13 @@ public class CattleCorral extends ApplicationAdapter {
 //	SpriteBatch batch;
 	ShapeRenderer shapeRenderer;
 	Texture img;
-	
+	private float[] vertices;
+
 	@Override
 	public void create () {
 //		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
+		vertices = new float[]{315,0,315,100,50,100,50,400,550,400,550,50,800,50,800,5,500,5,500,350,100,350,100,150,365,150,365,0};
 //		img = new Texture("badlogic.jpg");
 	}
 
@@ -28,6 +30,10 @@ public class CattleCorral extends ApplicationAdapter {
 //		batch.begin();
 //		batch.draw(img, 0, 0);
 //		batch.end();
+		shapeRenderer.begin(ShapeType.Line);
+		shapeRenderer.setColor(Color.BLACK);
+		shapeRenderer.polyline(vertices);
+		shapeRenderer.end();
 		shapeRenderer.begin(ShapeType.Filled);
 		shapeRenderer.setColor(Color.BLACK);
 		shapeRenderer.rect(325,5,15, 30);
