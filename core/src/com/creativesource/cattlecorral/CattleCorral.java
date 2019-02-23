283480ab771253cca8caf12bd848b88c3e0507da
Jantz <jantzillac.93@gmail.com>
2019-02-23 10:46:00 -0600

    Implement "Input Processor" & necessary methods

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -2,6 +2,7 @@ package com.creativesource.cattlecorral;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,7 +10,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
-public class CattleCorral extends ApplicationAdapter {
+public class CattleCorral extends ApplicationAdapter implements InputProcessor {
 //	SpriteBatch batch;
 	ShapeRenderer shapeRenderer;
 	Texture img;
@@ -45,5 +46,45 @@ public class CattleCorral extends ApplicationAdapter {
 //		batch.dispose();
 //		img.dispose();
 		shapeRenderer.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
