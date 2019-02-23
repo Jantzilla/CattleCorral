22b1bce10ed76a657a56fc6688229e7690f565b7
Jantz <jantzillac.93@gmail.com>
2019-02-23 11:04:20 -0600

    Update "camera" & render "tiledMapRenderer" inside "render()"

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -44,6 +44,9 @@ public class CattleCorral extends ApplicationAdapter implements InputProcessor {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 //		batch.begin();
 //		batch.draw(img, 0, 0);
 //		batch.end();
