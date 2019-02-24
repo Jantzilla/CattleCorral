62ee02cb6bd614825f56a38b51015c0787a32f71
Jantz <jantzillac.93@gmail.com>
2019-02-24 08:04:16 -0600

    Change "CattleCorral" & extend "Game" to launch new "Screens"

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -1,108 +1,21 @@
 package com.creativesource.cattlecorral;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.Game;
+import com.creativesource.cattlecorral.Constants.Difficulty;
 
-public class CattleCorral extends ApplicationAdapter implements InputProcessor {
-	ShapeRenderer shapeRenderer;
-	TiledMap tiledMap;
-	OrthogonalTiledMapRenderer tiledMapRenderer;
-	ExtendViewport extendViewport;
-	OrthographicCamera camera;
-	float worldWidth,worldHeight;
+
+public class CattleCorral extends Game {
 
 	@Override
-	public void create () {
-		float tileWidth=32,tileHeight=32;
-		float mapWidth=20,mapHeight=20;
-
-		worldWidth=tileWidth*mapWidth;
-		worldHeight=tileHeight*mapHeight;
-
-        camera = new OrthographicCamera();
-		extendViewport =new ExtendViewport(worldWidth,worldHeight,camera);
-        camera.update();
-        tiledMap = new TmxMapLoader().load("cattle_corral_test_map.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-		shapeRenderer = new ShapeRenderer();
-		Gdx.input.setInputProcessor(this);
+	public void create() {
+		showDifficultyScreen();
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		tiledMapRenderer.setView(camera);
-		tiledMapRenderer.render();
-		shapeRenderer.begin(ShapeType.Filled);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.rect(332,5,15, 30);
-		shapeRenderer.end();
-	}
-	
-	@Override
-	public void dispose () {
-		shapeRenderer.dispose();
-		tiledMapRenderer.dispose();
-		tiledMap.dispose();
+	public void showDifficultyScreen() {
+		setScreen(new StartScreen(this));
 	}
 
-	@Override
-	public void resize(int width, int height) {
-		extendViewport.update(width,height,false);
-		extendViewport.getCamera().position.set(worldWidth/2,worldHeight/2,0);
-		extendViewport.getCamera().update();
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
+	public void showIciclesScreen(Difficulty difficulty) {
+		setScreen(new PlayScreen(this, difficulty));
 	}
 }
