010c3d122fbeba544031dd36b885fccf3e147166
Jantz <jantzillac.93@gmail.com>
2019-02-24 00:22:34 -0600

    Change "Gdx.gl.glClearColor" red channel from "1" to "0"
    This change will initialize the game background to black.

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -48,7 +48,7 @@ public class CattleCorral extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
