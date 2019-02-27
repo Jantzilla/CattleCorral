5f2d9d1872e8c1a055e3a804d883975a5e52851b
Jantz <jantzillac.93@gmail.com>
2019-02-27 13:04:32 -0600

    Implement abstract method "isCellBlocked()" in Animal subclasses

--- core/src/com/creativesource/cattlecorral/Pig.java
+++ core/src/com/creativesource/cattlecorral/Pig.java
@@ -10,4 +10,18 @@ public class Pig extends Animal {
     public Pig(Animation up, Animation left, Animation down, Animation right, Viewport viewport, float worldWidth, ArrayList<TiledMapTileLayer> tiledMapTileLayers, int startPosition) {
         super(up, left, down, right, viewport, worldWidth, tiledMapTileLayers, startPosition);
     }
+
+    @Override
+    boolean isCellBlocked(float x, float y) {
+        for(TiledMapTileLayer tiledMapTileLayer : tiledMapTileLayers) {
+            if(tiledMapTileLayer.isVisible()) {
+                TiledMapTileLayer.Cell cell = tiledMapTileLayer.getCell((int) (x / tiledMapTileLayer.getTileWidth()), (int) (y / tiledMapTileLayer.getTileHeight()));
+                if(cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("blocked")) {
+                    lastDirection = "";
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
