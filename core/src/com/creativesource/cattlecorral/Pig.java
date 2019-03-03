94a7316c49ccfb49d62c65b339899d95f4382cf2
Jantz <jantzillac.93@gmail.com>
2019-03-03 02:00:55 -0600

    Add "screen" to constructor, pass to super & use to get "points"

--- core/src/com/creativesource/cattlecorral/Pig.java
+++ core/src/com/creativesource/cattlecorral/Pig.java
@@ -7,8 +7,8 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import java.util.ArrayList;
 
 public class Pig extends Animal {
-    public Pig(Animation up, Animation left, Animation down, Animation right, Viewport viewport, float worldWidth, ArrayList<TiledMapTileLayer> tiledMapTileLayers, int startPosition) {
-        super(up, left, down, right, viewport, worldWidth, tiledMapTileLayers, startPosition);
+    public Pig(PlayScreen screen, Animation up, Animation left, Animation down, Animation right, Viewport viewport, float worldWidth, ArrayList<TiledMapTileLayer> tiledMapTileLayers, int startPosition) {
+        super(screen, up, left, down, right, viewport, worldWidth, tiledMapTileLayers, startPosition);
     }
 
     @Override
@@ -16,6 +16,11 @@ public class Pig extends Animal {
         for(TiledMapTileLayer tiledMapTileLayer : tiledMapTileLayers) {
             if(tiledMapTileLayer.isVisible()) {
                 TiledMapTileLayer.Cell cell = tiledMapTileLayer.getCell((int) (x / tiledMapTileLayer.getTileWidth()), (int) (y / tiledMapTileLayer.getTileHeight()));
+                if(!getCorraled(cell)) {
+                    if (cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("pig")) {
+                        screen.points += 20;
+                    }
+                }
                 if(cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("blocked")) {
                     lastDirection = "";
                     return true;
--- core/src/com/creativesource/cattlecorral/Sheep.java
+++ core/src/com/creativesource/cattlecorral/Sheep.java
@@ -7,8 +7,8 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import java.util.ArrayList;
 
 public class Sheep extends Animal {
-    public Sheep(Animation up, Animation left, Animation down, Animation right, Viewport viewport, float worldWidth, ArrayList<TiledMapTileLayer> tiledMapTileLayers, int startPosition) {
-        super(up, left, down, right, viewport, worldWidth, tiledMapTileLayers, startPosition);
+    public Sheep(PlayScreen screen, Animation up, Animation left, Animation down, Animation right, Viewport viewport, float worldWidth, ArrayList<TiledMapTileLayer> tiledMapTileLayers, int startPosition) {
+        super(screen, up, left, down, right, viewport, worldWidth, tiledMapTileLayers, startPosition);
     }
 
     @Override
@@ -16,6 +16,11 @@ public class Sheep extends Animal {
         for(TiledMapTileLayer tiledMapTileLayer : tiledMapTileLayers) {
             if(tiledMapTileLayer.isVisible()) {
                 TiledMapTileLayer.Cell cell = tiledMapTileLayer.getCell((int) (x / tiledMapTileLayer.getTileWidth()), (int) (y / tiledMapTileLayer.getTileHeight()));
+                if(!getCorraled(cell)) {
+                    if (cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("sheep")) {
+                        screen.points += 20;
+                    }
+                }
                 if(cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("blocked")) {
                     lastDirection = "";
                     return true;
