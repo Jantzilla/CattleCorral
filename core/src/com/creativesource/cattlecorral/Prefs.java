f3a74d15002dc3b823f69dbbdf80d0fb9996611d
Jantz <jantzillac.93@gmail.com>
2019-03-05 11:22:10 -0600

    Create new class "Prefs"

---
+++ core/src/com/creativesource/cattlecorral/Prefs.java
@@ -0,0 +1,41 @@
+package com.creativesource.cattlecorral;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
+public class Prefs {
+
+    private Preferences pref ;
+    private boolean hasSound;
+    private int topScore;
+
+    public Prefs(){
+        pref = Gdx.app.getPreferences("Preferences");
+        hasSound = pref.getBoolean("hasSound",true);
+        topScore = pref.getInteger("topScore",0);
+
+    }
+
+    public void setSound(boolean hasSound){
+        this.hasSound=hasSound;
+        pref.putBoolean("hasSound",hasSound);
+        pref.flush();
+    }
+
+    public boolean hasSound(){
+        return hasSound;
+    }
+
+
+    public void increaseScore(int topScore){
+        if(topScore > getTopScore()) {
+            this.topScore = topScore;
+            pref.putInteger("topScore", topScore);
+            pref.flush();
+        }
+    }
+
+    public int getTopScore(){
+        return topScore;
+    }
+}
