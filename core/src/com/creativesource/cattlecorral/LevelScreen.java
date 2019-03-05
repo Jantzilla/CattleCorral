ce2a19feebc73763f69110e0f72928a43eb62ba1
Jantz <jantzillac.93@gmail.com>
2019-03-05 14:43:43 -0600

    Remove old "level button rect backgrounds" & label "GlyphLayout"

--- core/src/com/creativesource/cattlecorral/LevelScreen.java
+++ core/src/com/creativesource/cattlecorral/LevelScreen.java
@@ -177,61 +177,12 @@ public class LevelScreen extends InputAdapter implements Screen {
         Gdx.gl.glClearColor(Constants.BACKGROUND_COLOR.r, Constants.BACKGROUND_COLOR.g, Constants.BACKGROUND_COLOR.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        renderer.setProjectionMatrix(viewport.getCamera().combined);
-
-        renderer.begin(ShapeType.Filled);
-
-        renderer.setColor(Constants.EASY_COLOR);
-        renderer.rect(Constants.LEVEL_1[0], Constants.LEVEL_1[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_2[0], Constants.LEVEL_2[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_3[0], Constants.LEVEL_3[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_4[0], Constants.LEVEL_4[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_5[0], Constants.LEVEL_5[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_6[0], Constants.LEVEL_6[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_7[0], Constants.LEVEL_7[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_8[0], Constants.LEVEL_8[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_9[0], Constants.LEVEL_9[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-        renderer.rect(Constants.LEVEL_10[0], Constants.LEVEL_10[1], Constants.BUTTON_SIZE, Constants.BUTTON_SIZE);
-
-        renderer.end();
-
         batch.setProjectionMatrix(viewport.getCamera().combined);
 
         batch.begin();
 
         final GlyphLayout gameTitle = new GlyphLayout(font, "Levels");
         titleFont.draw(batch, "Levels", viewport.getWorldWidth() / 2 , (float) (viewport.getWorldHeight() / 1.2  + gameTitle.height / 2), 0, Align.center, false);
-
-        final GlyphLayout levelLabel1 = new GlyphLayout(font, Constants.LEVEL_1_LABEL);
-        font.draw(batch, Constants.LEVEL_1_LABEL, Constants.LEVEL_1[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_1[1] + Constants.BUTTON_SIZE / 2 + levelLabel1.height / 2, 0, Align.right, false);
-
-        final GlyphLayout levelLabel2 = new GlyphLayout(font, Constants.MEDIUM_LABEL);
-        font.draw(batch, Constants.LEVEL_2_LABEL, Constants.LEVEL_2[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_2[1] + Constants.BUTTON_SIZE / 2 + levelLabel2.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel3 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_3_LABEL, Constants.LEVEL_3[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_3[1] + Constants.BUTTON_SIZE / 2 + levelLabel3.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel4 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_4_LABEL, Constants.LEVEL_4[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_4[1] + Constants.BUTTON_SIZE / 2 + levelLabel4.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel5 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_5_LABEL, Constants.LEVEL_5[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_5[1] + Constants.BUTTON_SIZE / 2 + levelLabel5.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel6 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_6_LABEL, Constants.LEVEL_6[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_6[1] + Constants.BUTTON_SIZE / 2 + levelLabel6.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel7 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_7_LABEL, Constants.LEVEL_7[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_7[1] + Constants.BUTTON_SIZE / 2 + levelLabel7.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel8 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_8_LABEL, Constants.LEVEL_8[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_8[1] + Constants.BUTTON_SIZE / 2 + levelLabel8.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel9 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_9_LABEL, Constants.LEVEL_9[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_9[1] + Constants.BUTTON_SIZE / 2 + levelLabel9.height / 2, 0, Align.center, false);
-
-        final GlyphLayout levelLabel10 = new GlyphLayout(font, Constants.HARD_LABEL);
-        font.draw(batch, Constants.LEVEL_10_LABEL, Constants.LEVEL_10[0] + Constants.BUTTON_SIZE / 2, Constants.LEVEL_10[1] + Constants.BUTTON_SIZE / 2 + levelLabel10.height / 2, 0, Align.center, false);
-
 
         batch.end();
 
