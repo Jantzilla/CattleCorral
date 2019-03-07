baa6d7c4efd350e20c19b957ee78c7c23f7b7a94
Jantz <jantzillac.93@gmail.com>
2019-03-06 21:05:19 -0600

    Play audio "intro.wav" then on completion play "play.wav"

--- core/src/com/creativesource/cattlecorral/CattleCorral.java
+++ core/src/com/creativesource/cattlecorral/CattleCorral.java
@@ -1,14 +1,29 @@
 package com.creativesource.cattlecorral;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.creativesource.cattlecorral.Constants.Level;
 
 
 public class CattleCorral extends Game {
 
+	Music intro, play;
+
 	@Override
 	public void create() {
         showStartScreen();
+		intro = Gdx.audio.newMusic(Gdx.files.internal("intro.wav"));
+		play = Gdx.audio.newMusic(Gdx.files.internal("play.wav"));
+		intro.play();
+
+		intro.setOnCompletionListener(new Music.OnCompletionListener() {
+			@Override
+			public void onCompletion(Music music) {
+				play.setLooping(true);
+				play.play();
+			}
+		});
 	}
 
 	public void showStartScreen() {
