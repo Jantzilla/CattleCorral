d8aa057771d6ce7296d134ffa0f325d1b158b852
Jantz <jantzillac.93@gmail.com>
2019-03-10 20:07:02 -0500

    Remove unnecessary "import"

--- core/src/com/creativesource/cattlecorral/utils/Constants.java
+++ core/src/com/creativesource/cattlecorral/utils/Constants.java
@@ -2,7 +2,6 @@ package com.creativesource.cattlecorral.utils;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 
 
 public class Constants {
